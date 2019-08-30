df2411c5e4ce1b7b637206d182b311277af3baf3
sufeng <306406356@qq.com>
2019-08-30 16:04:03 +0800

    ss

---
+++ bomi/Web/tiaoshi.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Maticsoft.Web
+{
+    public partial class tiaoshi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('页面异常')</script>");
+        }
+    }
+}
